3f079a739cd7321fe5072c59f06ecbdf819aef1d
amin <aminqanbarii1992@gmail.com>
2022-03-28 03:28:44 -0700

    edit navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,12 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
+  display: flex;
 `;
 
 const Wrapper = styled.div`
   padding: 10px 20px;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
