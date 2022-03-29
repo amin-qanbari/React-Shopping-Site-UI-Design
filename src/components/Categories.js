3a79caab3caba8c8716621ce19a864d5d37e5912
amin <aminqanbarii1992@gmail.com>
2022-03-28 23:15:56 -0700

    create jsx categories

---
+++ src/components/Categories.js
@@ -0,0 +1,25 @@
+//styled-components
+import styled from "styled-components";
+
+//components
+import { categories } from "../data";
+import CategoryItem from "./CategoryItem";
+
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+`
+
+const Categories = () => {
+  return (
+    <Container>
+        {categories.map(item => (
+            <CategoryItem item={item}/>
+        ))}
+    </Container>
+  )
+}
+
+export default Categories
