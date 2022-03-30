e7facb89035fd59aad8f1a57fc7fe61adfe47205
amin <aminqanbarii1992@gmail.com>
2022-03-29 22:31:07 -0700

    add routes in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import Cart from './pages/Cart';
-import Login from './pages/Login';
-import Product from './pages/Product';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import ProductList from './pages/ProductList';
+import "./App.css";
+//components
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+//react-router-dom
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    <Login/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productList" element={<ProductList />} />
+
+      </Routes>
     </div>
   );
 }
