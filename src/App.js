73f36dbd9e50ab91d38e18c813d943ca9834d7af
amin <aminqanbarii1992@gmail.com>
2022-03-31 02:44:18 -0700

    i add shirt , tshirt and lightjackets routes to app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import Nightweare from "./pages/Nightweare";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Newsletter from "./components/Newsletter";
+import Tshirt from "./pages/Tshirt";
+import Shirt from "./pages/Shirt";
+import LightJacket from "./pages/LightJacket";
 
 
 //react-router-dom
@@ -30,7 +33,11 @@ function App() {
         <Route path="/summer" element={<Summer />} />
         <Route path="/autumn" element={<Autumn />} />
         <Route path="/nightwear" element={<Nightweare />} />
+        <Route path="/t-shirt" element={<Tshirt />} />
+        <Route path="/shirt" element={<Shirt />} />
+        <Route path="/lightJacket" element={<LightJacket />} />
         <Route path="/productList" element={<ProductList />} />
+
       </Routes>
       <Newsletter/>
       <Footer/>
