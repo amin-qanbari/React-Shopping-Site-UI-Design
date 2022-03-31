08e80a5cc02362542f3b552c2e0ba5ac622c30fd
amin <aminqanbarii1992@gmail.com>
2022-03-31 01:18:44 -0700

    edit app and add routes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Summer from "./pages/Summer";
+import Autumn from "./pages/Autumn";
+import Nightweare from "./pages/Nightweare";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Newsletter from "./components/Newsletter";
+
+
 //react-router-dom
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cart" element={<Cart/>} />
         <Route path="/product" element={<Product />} />
+        <Route path="/summer" element={<Summer />} />
+        <Route path="/autumn" element={<Autumn />} />
+        <Route path="/nightwear" element={<Nightweare />} />
         <Route path="/productList" element={<ProductList />} />
-
       </Routes>
+      <Newsletter/>
+      <Footer/>
     </div>
   );
 }
