1b58d5ec8c5c6778ba63ebad1c1c2e853798a31f
amin <aminqanbarii1992@gmail.com>
2022-04-03 02:04:40 -0700

    change products to popularProducts in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
 import Newsletter from '../components/Newsletter'
-import Products from '../components/Products'
+import PopularProducts from '../components/PopularProducts'
 import Slider from '../components/Slider'
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
         <Announcement/>
         <Slider/>
         <Categories/>
-        <Products/>
+        <PopularProducts/>
         <Newsletter/>
     </div>
   )
