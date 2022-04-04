6d33a91585f9c29079fa9c859b9490ec6667f28b
amin <aminqanbarii1992@gmail.com>
2022-04-04 04:02:54 -0700

    add useContext and searchContext and filter products in PopularProducts page

--- src/components/PopularProducts.js
+++ src/components/PopularProducts.js
@@ -1,41 +1,51 @@
 //usecontext
-// import { useContext } from "react"
+import { useContext } from "react";
 
 //styled-compnents
-import styled from "styled-components"
+import styled from "styled-components";
 
 //data
-import { popularProducts } from "../data"
+import { popularProducts } from "../data";
 
 //components
-import Product from "./Product"
+import Product from "./Product";
 
 //context
-// import { searchContext } from "../Context/SearchContextProvider"
-
+import { searchContext } from "../Context/SearchContextProvider";
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 const Title = styled.h2`
-padding: 0px 30px;
-`
+  padding: 0px 30px;
+`;
 const PopularProducts = () => {
-  
-  // const searchTerm = useContext(searchContext)
-  return (<>
-    <Title>Popular Products :</Title>
-    <Container>
+  const searchTerm = useContext(searchContext);
+  return (
+    <>
+      <Title>Popular Products :</Title>
+      <Container>
         {popularProducts
-        .map(item => (
-            <Product item={item} key={item.id}/>
-        ) )}
-    </Container>
+          .filter((val) => {
+            if (searchTerm === "") {
+              return val;
+            } else if (
+              val.title.toLowerCase().includes(searchTerm.toLowerCase())
+            ) {
+              return val;
+            } else {
+              return false;
+            }
+          })
+          .map((item) => (
+            <Product item={item} key={item.id} />
+          ))}
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default PopularProducts
+export default PopularProducts;
