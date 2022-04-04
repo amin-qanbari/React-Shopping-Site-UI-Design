6a26ef49ee90cd6e34a8ba256d6dd583edf07299
amin <aminqanbarii1992@gmail.com>
2022-04-04 04:03:47 -0700

    add useContext and searchContext and filter products in Nightweare page

--- src/pages/Nightweare.js
+++ src/pages/Nightweare.js
@@ -1,19 +1,38 @@
-import React from "react";
+//usecontext
+import { useContext } from "react";
+
+//context
+import { searchContext } from "../Context/SearchContextProvider";
+
 import Product from "../components/Product";
 import { nightWeare } from "../data";
-import styled from "styled-components"
+import styled from "styled-components";
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-`
+  padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 const Nightweare = () => {
+  const searchTerm = useContext(searchContext);
+
   return (
     <Container>
-      {nightWeare.map((item) => (
-        <Product item={item} key={item.id} />
-      ))}
+      {nightWeare
+        .filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (
+            val.title.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return val;
+          } else {
+            return false;
+          }
+        })
+        .map((item) => (
+          <Product item={item} key={item.id} />
+        ))}
     </Container>
   );
 };
