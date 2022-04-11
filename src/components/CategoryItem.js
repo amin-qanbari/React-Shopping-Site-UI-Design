5c7eefffcc566e2ce1e7156b8fa3a06cc1321c36
amin <aminqanbarii1992@gmail.com>
2022-04-11 03:08:31 -0700

    responsive this component

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,21 +1,29 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-//responsive
-import { mobile } from "../responsive";
-
 const Container = styled.div`
   flex: 1;
   margin: 3px;
   height: 70vh;
   position: relative;
+
+  @media (max-width: 768px) {
+    height: 60vh;
+  }
+
+  @media (max-width : 576px) {
+    height: 50vh;
+  }
 `;
 
 const Image = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-  ${mobile({ height: "20vh" })}
+
+@media (max-width : 380px) {
+  height: 50vh;
+}
 `;
 
 const Info = styled.div`
@@ -33,15 +41,51 @@ const Info = styled.div`
 const Title = styled.h1`
   color: white;
   margin-bottom: 20px;
+  @media (max-width: 992px) {
+    font-size: 24px;
+}
+
+@media (max-width: 768px) {
+  font-size: 22px;
+}
+
+@media (max-width : 576px) {
+  font-size: 20px;
+}
+
+@media (max-width : 380px) {
+  font-size: 24px;
+}
 `;
 
 const Linked = styled(Link)`
   border: none;
   background-color: white;
+  text-decoration: none;
   color: gray;
   cursor: pointer;
   padding: 10px;
   font-weight: 600;
+
+  @media (max-width: 992px) {
+    padding: 16px;
+    padding: 8px;
+}
+
+@media (max-width: 768px) {
+  font-size: 14px;
+  padding: 6px;
+}
+
+@media (max-width : 576px) {
+  font-size: 12px;
+  padding: 4px;
+}
+
+@media (max-width : 380px) {
+  font-size: 14px;
+  padding: 6px;
+}
 `;
 
 const CategoryItem = ({ item }) => {
