2613d9e60da3005400c398f6835e77df4a618309
amin <aminqanbarii1992@gmail.com>
2022-04-11 10:00:57 -0700

    edit footer route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Summer from "./pages/Summer";
 import Autumn from "./pages/Autumn";
 import Nightweare from "./pages/Nightweare";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import Tshirt from "./pages/Tshirt";
 import Shirt from "./pages/Shirt";
 import LightJacket from "./pages/LightJacket";
